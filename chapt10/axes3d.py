5b2789676fb63d1913bac78931975e3f2aa1b2d2
BenSmith <ben@mech.(none)>
2009-07-19 19:35:01 -0700

    add chapter 10 axes3d.py

---
+++ chapt10/axes3d.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# Demonstrates an immersive 3D environment using actors and a camera
+# This version adds lights and material properties and shadows.
+# Ben Smith
+# benjamin.coder.smith@gmail.com
+#
+# Based heavily on: sphereworld.cpp
+# OpenGL SuperBible
+# Program by Richard S. Wright Jr.
+
+import pyglet
+from pyglet.gl import *
+from pyglet import window
+from pyglet.window import key
+from random import randint
+
+import sys
+sys.path.append("../shared")
+from forpyglet import gltDrawUnitAxes
+
+xRot = 0.0
+yRot = 0.0
+
+
+class MainWindow(window.Window):
+    def __init__(self, *args, **kwargs):
+        window.Window.__init__(self, *args, **kwargs)
+
+        whiteLight = (GLfloat * 4)(0.05, 0.05, 0.05, 1.0)
+        sourceLight = (GLfloat * 4)(0.25, 0.25, 0.25, 1.0)
+        lightPos = (GLfloat * 4)(-10.0, 5.0, 5.0, 1.0)
+        
+        glEnable(GL_DEPTH_TEST) # Hidden surface removal
+        glFrontFace(GL_CCW) # Counter clock-wise polygons face out
+        glEnable(GL_CULL_FACE) 
+        
+        glEnable(GL_LIGHTING)
+
+        # Setup and enable light 0
+        glLightModelfv(GL_LIGHT_MODEL_AMBIENT,whiteLight)
+        glLightfv(GL_LIGHT0,GL_AMBIENT,sourceLight)
+        glLightfv(GL_LIGHT0,GL_DIFFUSE,sourceLight)
+        glLightfv(GL_LIGHT0,GL_POSITION,lightPos)
+        glEnable(GL_LIGHT0)
+
+        # Enable color tracking
+        glEnable(GL_COLOR_MATERIAL)
+        
+        # Set Material properties to follow glColor values
+        glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
+
+        # Black background
+        glClearColor(0.0, 0.0, 0.0, 1.0 )
+
+        
+    # Called to draw scene
+    def on_draw(self):
+        # Clear the window with the current clearing color
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        
+        # Save the matrix state and do the rotations
+        glPushMatrix()
+        
+        # Move object back and do in place rotation
+        glTranslatef(0.0, 0.0, -5.0)
+        glRotatef(xRot, 1.0, 0.0, 0.0)
+        glRotatef(yRot, 0.0, 1.0, 0.0)
+
+        # Draw something
+        gltDrawUnitAxes()
+            
+        # Restore the matrix state
+        glPopMatrix()
+
+    # Called when the window has changed size (including when the window is created)
+    def on_resize(self, w, h):
+        # Prevent a divide by zero
+        if h == 0:
+            h = 1
+        
+        # Set Viewport to window dimensions
+        glViewport(0, 0, w, h)
+        
+        fAspect = float(w) / float(h)
+        
+        # Reset the coordinate system before modifying
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+
+        # Set the clipping volume
+        gluPerspective(35.0, fAspect, 1.0, 40.0)
+
+        # Reset Model view matrix stack
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        
+    def on_key_press(self, symbol, modifier):
+        global xRot, yRot
+        if symbol == key.UP:
+            xRot -= 5.0
+        elif symbol == key.DOWN:
+            xRot += 5.0
+        elif symbol == key.LEFT:
+            yRot -= 5.0
+        elif symbol == key.RIGHT:
+            yRot += 5.0
+        
+        xRot = float(int(xRot) % 360)
+        yRot = float(int(yRot) % 360)
+
+# Main program entry point
+if __name__ == '__main__':
+    w = MainWindow(800, 600, caption='Unit Axis', resizable=True)
+    pyglet.app.run()
+
--- shared/forpyglet.py
+++ shared/forpyglet.py
@@ -56,4 +56,72 @@ def gltDrawTorus(majorRadius, minorRadius, numMajor, numMinor):
         i += 1
 
 def gltDrawSphere(a, b, c):
-    pass
+    glutSolidSphere(a, b, c)
+    
+# Draw a 3D unit Axis set
+# Draw the unit axis. A small white sphere represents the origin
+# and the three axes are colored Red, Green, and Blue, which 
+# corresponds to positive X, Y, and Z respectively. Each axis has
+# an arrow on the end, and normals are provided should the axes
+# be lit. These are all built using the quadric shapes. For best
+# results, put this in a display list.
+def gltDrawUnitAxes():
+    # Measurements
+    fAxisRadius = 0.025
+    fAxisHeight = 1.0
+    fArrowRadius = 0.06
+    fArrowHeight = 0.1
+
+    # Setup the quadric object
+    pObj = gluNewQuadric()
+    gluQuadricDrawStyle(pObj, GLU_FILL)
+    gluQuadricNormals(pObj, GLU_SMOOTH)
+    gluQuadricOrientation(pObj, GLU_OUTSIDE)
+    gluQuadricTexture(pObj, GLU_FALSE)
+
+    ###########################/
+    # Draw the blue Z axis first, with arrowed head
+    glColor3f(0.0, 0.0, 1.0)
+    gluCylinder(pObj, fAxisRadius, fAxisRadius, fAxisHeight, 10, 1)
+    glPushMatrix()
+    glTranslatef(0.0, 0.0, 1.0)
+    gluCylinder(pObj, fArrowRadius, 0.0, fArrowHeight, 10, 1)
+    glRotatef(180.0, 1.0, 0.0, 0.0)
+    gluDisk(pObj, fAxisRadius, fArrowRadius, 10, 1)
+    glPopMatrix()
+
+    ###########################/
+    # Draw the Red X axis 2nd, with arrowed head
+    glColor3f(1.0, 0.0, 0.0)
+    glPushMatrix()
+    glRotatef(90.0, 0.0, 1.0, 0.0)
+    gluCylinder(pObj, fAxisRadius, fAxisRadius, fAxisHeight, 10, 1)
+    glPushMatrix()
+    glTranslatef(0.0, 0.0, 1.0)
+    gluCylinder(pObj, fArrowRadius, 0.0, fArrowHeight, 10, 1)
+    glRotatef(180.0, 0.0, 1.0, 0.0)
+    gluDisk(pObj, fAxisRadius, fArrowRadius, 10, 1)
+    glPopMatrix()
+    glPopMatrix()
+
+    ###########################/
+    # Draw the Green Y axis 3rd, with arrowed head
+    glColor3f(0.0, 1.0, 0.0)
+    glPushMatrix()
+    glRotatef(-90.0, 1.0, 0.0, 0.0)
+    gluCylinder(pObj, fAxisRadius, fAxisRadius, fAxisHeight, 10, 1)
+    glPushMatrix()
+    glTranslatef(0.0, 0.0, 1.0)
+    gluCylinder(pObj, fArrowRadius, 0.0, fArrowHeight, 10, 1)
+    glRotatef(180.0, 1.0, 0.0, 0.0)
+    gluDisk(pObj, fAxisRadius, fArrowRadius, 10, 1)
+    glPopMatrix()
+    glPopMatrix()
+
+    ############################
+    # White Sphere at origin
+    glColor3f(1.0, 1.0, 1.0)
+    gluSphere(pObj, 0.05, 15, 15)
+
+    # Delete the quadric
+    gluDeleteQuadric(pObj)
